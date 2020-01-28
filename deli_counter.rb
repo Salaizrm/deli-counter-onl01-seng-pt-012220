9cccebeb3b1e357aec2c8164b0a2cc3840d6a90c
Robert Salaiz <work.salaizrm@gmail.com>
2020-01-28 22:26:42 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,7 @@ katz_deli = []
 def line (katz_deli)
 
   line = 0
-  customer.collect do |name|
+  katz_deli.collect do |name|
   line += 1
   print "Welcome, #{name}. You are number #{line} in line."
   end
