42cecb8a8fc3171c09e6e1d8045321875b5f386b
Robert Salaiz <work.salaizrm@gmail.com>
2020-01-28 22:41:31 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,24 +16,14 @@ end
 
 def line (array)
   if array.length >= 1
-    nuarray = []
+    new_array = []
     counter = 1 
     array.each do |name|
-      nuarray.push("#{counter}. #{name}")
+      new_array.push("#{counter}. #{name}")
       counter += 1 
     end 
-    puts "The line is currently: #{nuarray.join(" ")}"
+    puts "The line is currently: #{new_array.join(" ")}"
   else
     puts "The line is currently empty."
   end
 end
-  
-  
- if array.size == 1 
-    return array.first
-  elsif array.size == 2
-    return array.join(" and ")
-  else
-    return array [0..-2].join(', ') + ", and " + array.last
-  end
-    
