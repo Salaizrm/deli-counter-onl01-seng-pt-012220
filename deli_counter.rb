e7519a6d7a11a01c6d00fc9fd6c24df0b0f84fa1
Robert Salaiz <work.salaizrm@gmail.com>
2020-01-28 23:17:49 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -33,6 +33,13 @@ end
 def take_a_number( katz_deli, name )
   
   katz_deli << name 
-  puts 
+  line = 0
+  katz_deli.collect do |name|
+  line += 1
+  puts "Welcome, #{name}. You are number #{line} in line."
+  end
+  
+  if line == 0
+    return puts "The line is currently empty."
     
 end
