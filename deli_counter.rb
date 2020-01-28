3e5eefd3d00328b655938c11671fec01ade49532
Robert Salaiz <work.salaizrm@gmail.com>
2020-01-28 22:27:50 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,6 +2,12 @@ katz_deli = []
 
 def line (katz_deli)
 
+  line = 0
+  katz_deli.collect do |name|
+  line += 1
+  puts "Welcome, #{name}. You are number #{line} in line."
+  end
+  
   if line == 0
     return puts "The line is currently empty."
   end
